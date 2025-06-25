774b619246860658f9d0dd00aca4193aedfa0455
CHUCHUCHUCHU <raikyu70@gmail.com>
2025-06-25 16:06:27 +0900

    fix: 02

--- Question_01_10/problem/02.py
+++ Question_01_10/problem/02.py
@@ -8,9 +8,9 @@ Que1_path = os.path.dirname(os.path.dirname(__file__))
 img = cv2.imread(os.path.join(Que1_path, "imori.jpg")).astype(np.float32)
 
 print(img.shape)
-grayimg = (img[:, :, 0] * 0.2126 +
-           img[:, :, 1] * 0.7152 +
-           img[:, :, 2] * 0.0722).astype(np.uint8)
+grayimg = (img[..., 0] * 0.2126 +
+           img[..., 1] * 0.7152 +
+           img[..., 2] * 0.0722).astype(np.uint8)
 
 
 cv2.imshow("gray", grayimg)
